339df9538194fa3ac53ae4ba900caf765b779cd6
npaudyal321 <naween321@gmail.com>
2023-03-14 10:08:54 +0545

    exception demo

--- classworks/class3/exception.py
+++ classworks/class3/exception.py
@@ -5,6 +5,7 @@ def addition(a, b):
     except TypeError:
         return "Please enter data with same type"
 
+
 first_num = input("Enter a number")
 second_num = input("Enter another number")
 r = addition(first_num, second_num)
